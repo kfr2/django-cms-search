2847d50873aa2d3b0b451650dc37943c32c90e2b
Kevin Richardson <kevin@magically.us>
2013-05-29 09:04:53 -0400

    Enable search apphook

--- cms_search/cms_app.py
+++ cms_search/cms_app.py
@@ -2,8 +2,10 @@ from django.conf.urls.defaults import patterns, url
 from django.utils.translation import ugettext_lazy as _
 
 from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
 
 from haystack.views import search_view_factory
+
 
 class HaystackSearchApphook(CMSApp):
     name = _("search apphook")
@@ -11,3 +13,5 @@ class HaystackSearchApphook(CMSApp):
         url('^$', search_view_factory(), name='haystack-search'),
     ),]
 
+
+apphook_pool.register(HaystackSearchApphook)
